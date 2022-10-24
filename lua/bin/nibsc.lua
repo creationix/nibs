4951360a18e67852a842dcb6848c17a95ce2debf
Tim Caswell <creationix@vercel.com>
2022-10-24 18:00:57 -0500

    More cleanup of lua version

--- lua/bin/nibsc.lua
+++ lua/bin/nibsc.lua
@@ -5,12 +5,12 @@ local p = require('pretty-print').prettyPrint
 local Nibs = require 'nibs'
 local fs = require 'coro-fs'
 local luvi = require 'luvi'
-local Json = require 'ordered-json'
+local Tibs = require 'tibs'
 
 local input_path = assert(process.argv[2], "Missing input path as first argument")
 local output_path = assert(process.argv[3], "Missing output path as second argument")
 print("\nLoading and parsing input file '" .. input_path .. "'...")
-local input = assert(Json.decode(assert(fs.readFile(input_path))))
+local input = assert(Tibs.decode(assert(fs.readFile(input_path))))
 -- p(input)
 
 print("Marking containers larger than 10 for indexing...")
