fd2c80f3f7d2d66e194d88e289d4ea62931ae658
Tim Caswell <creationix@vercel.com>
2022-10-28 18:23:44 -0500

    Cleanup and use faster xxhash library

--- js/symbols.js
+++ js/symbols.js
@@ -1,4 +1,6 @@
+// This is set to encode References, the value is the ID
 export const isRef = Symbol("isRef")
+// This is set to boolean to mark an array as a scope
 export const isScope = Symbol("isScope")
+// This is set to boolean true to mark a JavaScript Array or Map to Tibs Array or Trie
 export const isIndexed = Symbol("isIndexed")
-export const toTibs = Symbol("toTibs")
