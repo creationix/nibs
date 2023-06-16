bc971a345bf2a881690f5896e91c41d6c27b311d
Tim Caswell <tim@creationix.com>
2023-06-15 21:28:12 -0500

    Save progress

--- c/tibs.c
+++ c/tibs.c
@@ -1,191 +1,168 @@
-#include <stdint.h>
-#include <string.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
+// Creator:  creationix
+// Maintainer: creationix
+// Last Change: 2023-06-16T02:09:39.855Z
+// Description: A parser for the TIBS format
 
-typedef void (*tibs_on_slice)(void* state, const char* start, const char* end);
-typedef void (*tibs_on_start)(void* state, const char* start);
-
-struct tibs_events {
-    tibs_on_slice on_number;
-    tibs_on_start on_boolean;
-    tibs_on_start on_null;
-    tibs_on_slice on_bytes;
-    tibs_on_slice on_string;
-    tibs_on_start on_map_start;
-    tibs_on_start on_map_stop;
-    tibs_on_start on_list_start;
-    tibs_on_start on_list_stop;
-    tibs_on_start on_scope_start;
-    tibs_on_start on_scope_stop;
+enum tibs_type {
+    TIBS_NULL,
+    TIBS_BOOLEAN,
+    TIBS_NUMBER,
+    TIBS_BYTES,
+    TIBS_STRING,
+    TIBS_REF,
+    TIBS_MAP_BEGIN,
+    TIBS_MAP_END,
+    TIBS_LIST_BEGIN,
+    TIBS_LIST_END,
+    TIBS_SCOPE_BEGIN,
+    TIBS_SCOPE_END,
+    TIBS_EOS
 };
 
-static bool starts_with(const char* prefix, const char* start, const char* end) {
-    size_t len = end - start;
-    return strlen(prefix) <= len && memcmp(prefix, start, len) == 0;
+// Check if one null terminated string begins with another
+static int starts_with(const char* prefix, const char* str) {
+    while (*prefix && *prefix == *str) {
+        prefix++;
+        str++;
+    }
+    return *prefix == 0;
 }
 
-void tibs_parse(struct tibs_events* events, const char* start, size_t len, void* state) {
-    const char* pos = start;
-    const char* end = start + len;
-    while (pos <= end) {
-        if (starts_with("null", pos, end)) {
-            events->on_null(state, pos);
-            pos += 4;
-            continue;
+struct tibs_token {
+    enum tibs_type type;
+    int offset;
+    int len;
+};
+
+typedef struct tibs_token token_t;
+
+// `tibs` is a null terminated string
+// `offset` is an offset into that string to start parsing
+token_t tibs_parse(const char* tibs, int offset) {
+    for (;tibs[offset];offset++) {
+        if (starts_with("null", tibs + offset)) {
+            return (token_t){TIBS_NULL, offset, 4};
         }
-        if (starts_with("true", pos, end)) {
-            events->on_boolean(state, pos);
-            pos += 4;
-            continue;
+        if (starts_with("true", tibs + offset)) {
+            return (token_t){TIBS_BOOLEAN, offset, 4};
         }
-        if (starts_with("false", pos, end)) {
-            events->on_boolean(state, pos);
-            pos += 5;
-            continue;
+        if (starts_with("false", tibs + offset)) {
+            return (token_t){TIBS_BOOLEAN, offset, 5};
         }
-        if (starts_with("-inf", pos, end)) {
-            events->on_number(state, pos, pos + 4);
-            pos += 4;
-            continue;
+        if (starts_with("-inf", tibs + offset)) {
+            return (token_t){TIBS_NUMBER, offset, 4};
         }
-        if (starts_with("inf", pos, end)) {
-            events->on_number(state, pos, pos + 3);
-            pos += 3;
-            continue;
+        if (starts_with("inf", tibs + offset)) {
+            return (token_t){TIBS_NUMBER, offset, 3};
         }
-        if (starts_with("nan", pos, end)) {
-            events->on_number(state, pos, pos + 3);
-            pos += 3;
-            continue;
+        if (starts_with("nan", tibs + offset)) {
+            return (token_t){TIBS_NUMBER, offset, 3};
         }
-        if (pos[9] == '[') {
-            events->on_list_start(state, pos);
-            pos += 10;
-            continue;
-        }
-        switch(*pos) {
-            case '[':
-                events->on_list_start(state, pos++);
-                continue;
-            case ']':
-                events->on_list_stop(state, pos++);
-                continue;
-            case '{':
-                events->on_map_start(state, pos++);
-                continue;
-            case '}':
-                events->on_map_stop(state, pos++);
-                continue;
-            case '(':
-                events->on_scope_start(state, pos++);
-                continue;
-            case ')':
-                events->on_scope_stop(state, pos++);
-                continue;
-            case '0': case '1': case '2': case '3': case '4':
-            case '5': case '6': case '7': case '8': case '9':
-            case '-': {
-                if (pos + 4 <= end && pos[1] == 'i' && pos[2] == 'n' && pos[3] == 'f') {
-                    events->on_number(state, pos, pos + 4);
-                    pos += 4;
-                    continue;
-                }
-                const char* pos2 = pos + 1;
-                start: switch(*pos2) {
-                    case '0': case '1': case '2': case '3': case '4':
-                    case '5': case '6': case '7': case '8': case '9':
-                    case '-': case '+': case 'e': case 'E': case '.':
-                        pos2++;
-                        goto start;
-                }
-                events->on_number(state, pos, pos2);
-                pos = pos2;
-                continue;
+        if (tibs[offset] == '-' || (tibs[offset] >= '0' && tibs[offset] <= '9')) {
+            int len = 1;
+            while (tibs[offset + len] >= '0' && tibs[offset + len] <= '9') {
+                len++;
             }
-            case 'i': 
-                if (pos + 3 <= end && pos[1] == 'n' && pos[2] == 'f') {
-                    events->on_number(state, pos, pos + 3);
-                    pos += 3;
-                    continue;
+            if (tibs[offset + len] == '.') {
+                len++;
+                while (tibs[offset + len] >= '0' && tibs[offset + len] <= '9') {
+                    len++;
                 }
-            case 't':
-                events->on_boolean(state, pos);
-                pos += 4;
-                continue;
-            case 'f':
-                events->on_boolean(state, pos);
-                pos += 5;
-                continue;
-            case 'n':
-                if (pos + 3 <= end && pos[1] == 'a' && pos[2] == 'n') {
-                    events->on_number(state, pos, pos + 3);
-                    pos += 3;
-                    continue;
-                }
-                events->on_null(state, pos);
-                pos += 4;
-                continue;
-            case '<': {
-                const char* pos2 = pos + 1;
-                while (pos2 < end && *pos2++ != '>');
-                events->on_bytes(state, pos, pos2);
-                pos = pos2;
-                continue;
             }
-            case '"': {
-                const char* pos2 = pos + 1;
-                while (pos2 <= end) {
-                    if (*pos2 == '\\') {
-                        pos2 += 2;
-                        if (pos2 >= end) break;
-                    }
-                    if (*pos2++ == '"') break;
+            if (tibs[offset + len] == 'e' || tibs[offset + len] == 'E') {
+                len++;
+                if (tibs[offset + len] == '-' || tibs[offset + len] == '+') {
+                    len++;
                 }
-                events->on_string(state, pos, pos2);
-                pos = pos2;
-                continue;
+                while (tibs[offset + len] >= '0' && tibs[offset + len] <= '9') {
+                    len++;
+                }
             }
-            default:
-                pos++;
-                continue;
+            return (token_t){TIBS_NUMBER, offset, len};
+        }
+        if (tibs[offset] == '"') {
+            int len = 1;
+            while (tibs[offset + len] != '"') {
+                if (tibs[offset + len] == '\\') {
+                    len++;
+                }
+                len++;
+            }
+            return (token_t){TIBS_STRING, offset, len + 1};
+        }
+        if (tibs[offset] == '<') {
+            int len = 1;
+            while (tibs[offset + len] != '>') {
+                len++;
+            }
+            return (token_t){TIBS_BYTES, offset, len + 1};
+        }
+        if (tibs[offset] == '&') {
+            int len = 1;
+            while (tibs[offset + len] >= '0' && tibs[offset + len] <= '9') {
+                len++;
+            }
+            return (token_t){TIBS_REF, offset, len};
+        }
+        if (tibs[offset] == '[') {
+            int len = tibs[offset + 1] == '#' ? 2 : 1;
+            return (token_t){TIBS_LIST_BEGIN, offset, len};
+        }
+        if (tibs[offset] == ']') {
+            return (token_t){TIBS_LIST_END, offset, 1};
+        }
+        if (tibs[offset] == '{') {
+            int len = tibs[offset + 1] == '#' ? 2 : 1;
+            return (token_t){TIBS_MAP_BEGIN, offset, len};
+        }
+        if (tibs[offset] == '}') {
+            return (token_t){TIBS_MAP_END, offset, 1};
+        }
+        if (tibs[offset] == '(') {
+            return (token_t){TIBS_SCOPE_BEGIN, offset, 1};
+        }
+        if (tibs[offset] == ')') {
+            return (token_t){TIBS_SCOPE_END, offset, 1};
         }
     }
+    return (token_t){.type = TIBS_EOS, .offset = offset, .len = 0};
 }
 
 static void on_number(void* state, const char* start, const char* end) {
-    printf("on_number %.*s\n", (int)(end - start), start);
+    printf("on_number      %.*s\n", (int)(end - start), start);
 }
 static void on_boolean(void* state, const char* start) {
-    printf("on_boolean %.*s\n", *start == 't' ? 4 : 5, start);
+    printf("on_boolean     %.*s\n", *start == 't' ? 4 : 5, start);
 }
 static void on_null(void* state, const char* start) {
-    printf("on_null %.*s\n", 4, start);
+    printf("on_null        %.*s\n", 4, start);
 }
 static void on_bytes(void* state, const char* start, const char* end) {
-    printf("on_bytes %.*s\n", (int)(end - start), start);
+    printf("on_bytes       %.*s\n", (int)(end - start), start);
 }
 static void on_string(void* state, const char* start, const char* end) {
-    printf("on_string %.*s\n", (int)(end - start), start);
+    printf("on_string      %.*s\n", (int)(end - start), start);
 }
-static void on_map_start(void* state, const char* start) {
-    printf("on_map_start %p\n", start);
+static void on_ref(void* state, const char* start, const char* end) {
+    printf("on_ref         %.*s\n", (int)(end - start), start);
+}
+static void on_map_start(void* state, const char* start, const char* end) {
+    printf("on_map_start   %.*s\n", (int)(end - start), start);
 }
 static void on_map_stop(void* state, const char* start) {
-    printf("on_map_stop %p\n", start);
+    printf("on_map_stop    %1s\n", start);
 }
-static void on_list_start(void* state, const char* start) {
-    printf("on_list_start %p\n", start);
+static void on_list_start(void* state, const char* start, const char* end) {
+    printf("on_list_start  %.*s\n", (int)(end - start), start);
 }
 static void on_list_stop(void* state, const char* start) {
-    printf("on_list_stop %p\n", start);
+    printf("on_list_stop   %1s\n", start);
 }
 static void on_scope_start(void* state, const char* start) {
-    printf("on_scope_start %p\n", start);
+    printf("on_scope_start %1s\n", start);
 }
 static void on_scope_stop(void* state, const char* start) {
-    printf("on_scope_stop %p\n", start);
+    printf("on_scope_stop  %1s\n", start);
 }
 
 int main() {
@@ -195,6 +172,7 @@ int main() {
         .on_null = on_null,
         .on_bytes = on_bytes,
         .on_string = on_string,
+        .on_ref = on_ref,
         .on_map_start = on_map_start,
         .on_map_stop = on_map_stop,
         .on_list_start = on_list_start,
