348788cb981c0c35c0412c30277db501dce06f33
Tim Caswell <creationix@vercel.com>
2023-07-13 16:31:29 +0000

    Add more types to js nibs

--- js/index.d.ts
+++ js/index.d.ts
@@ -1,4 +1,12 @@
 declare module "nibs" {
-    export function optimize(doc: any, indexLimit: number, refs?:Map<any,number>) : any;
-    export function encode(val: any) : Uint8Array;
+    // Look for refs in the object and replace them with nibs refs.
+    // Also mark large array/objects to use indexes
+    // Returns a new object with the refs and indexes
+    export function optimize(doc: any, indexLimit: number, refs?: Map<any, number>): any
+    // Convert a JS object to a nibs binary
+    export function encode(val: any): Uint8Array
+    // Convert Objects back to maps and assume integers are integers and boolean is boolean.
+    export function toMap(obj: object): Map<any, any>
+    // Decode nibs binary to a JS object (with lazy reads)
+    export function decode(buf: Uint8Array): any
 }
