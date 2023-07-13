61cb7818f043cf33056dbb095baa8c851e559fc0
Tim Caswell <creationix@vercel.com>
2023-07-13 19:27:04 +0000

    Cleanup the exports and type defs for JS

--- js/index.d.ts
+++ js/index.d.ts
@@ -1,12 +1,17 @@
 declare module "nibs" {
+    // Convert a JS object to a nibs binary
+    export function encode(val: any): Uint8Array
+    // Decode nibs binary to a JS object (with lazy reads)
+    export function decode(buf: Uint8Array): any
     // Look for refs in the object and replace them with nibs refs.
     // Also mark large array/objects to use indexes
     // Returns a new object with the refs and indexes
-    export function optimize(doc: any, indexLimit: number, refs?: Map<any, number>): any
-    // Convert a JS object to a nibs binary
-    export function encode(val: any): Uint8Array
+    // If indexLimit and refs are omitted, automatic values will be provided.
+    export function optimize(doc: any, indexLimit?: number, refs?: Map<any, number>): any
     // Convert Objects back to maps and assume integers are integers and boolean is boolean.
     export function toMap(obj: object): Map<any, any>
-    // Decode nibs binary to a JS object (with lazy reads)
-    export function decode(buf: Uint8Array): any
+    // Parse a JSON or Tibs string with optional filename for error messages
+    export function fromTibs(tibs: string, filename?: string): any
+    // Encode a JS value to a Tibs string
+    export function toTibs(val: any): string
 }
--- js/index.js
+++ js/index.js
@@ -4,5 +4,6 @@ import * as Tibs from './tibs.js'
 export const encode = Nibs.encode
 export const decode = Nibs.decode
 export const optimize = Nibs.optimize
+export const toMap = Nibs.toMap
 export const fromTibs = Tibs.decode
 export const toTibs = Tibs.encode
