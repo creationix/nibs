9e75c01f64cdaacb96ca97b2e61030d9deb20c08
Tim Caswell <tim@creationix.com>
2023-08-02 00:08:48 -0500

    Fix tokenizer test

--- reverse/test-next-json-token.lua
+++ reverse/test-next-json-token.lua
@@ -10,7 +10,7 @@ local tests = {
         { "number", 3, 4 },
         { ",", 4, 5 },
         { "number", 5, 6 },
-        { "]", 6, 7 } 
+        { "]", 6, 7 }
     },
     '[ 1, 2, 3 ]', {
         { "[", 0, 1 },
@@ -25,7 +25,28 @@ local tests = {
         { "name": "Nibs",
           "open-license": true
         }
-    ]], {},
+    ]], {
+        { "{", 8, 9 },
+        { "string", 10, 16 },
+        { ":", 16, 17 },
+        { "string", 18, 24 },
+        { ",", 24, 25 },
+        { "string", 36, 50 },
+        { ":", 50, 51 },
+        { "true", 52, 56 },
+        { "}", 65, 66 },
+    },
+    " <1234> ", {
+        { "bytes", 1, 7 },
+    },
+    '"1\\"2"', {
+        { "string", 0, 6 },
+    },
+    '1 1.0 0.1', {
+        { "number", 0, 1 },
+        { "number", 2, 5 },
+        { "number", 6, 9 },
+    }
 }
 
 for i = 1, #tests, 2 do
@@ -40,12 +61,13 @@ for i = 1, #tests, 2 do
         assert(offset < limit)
         local t, o, l = ReverseNibs.next_json_token(json, offset, limit)
         assert(t and o and l)
-        p({ offset, limit }, { t, o, l }, result, ffi.string(json, o, l-o))
+        p({ offset, limit }, { t, o, l }, result, ffi.string(json + o, l - o))
         assert(t == result[1], "mismatched token in result")
         assert(o == result[2], "mismatched offset in result")
         assert(l == result[3], "mismatched limit in result")
         offset = l
     end
     local t, o, l = ReverseNibs.next_json_token(json, offset, limit)
+    p(t,o,l)
     assert(not t and not o and not l)
 end
